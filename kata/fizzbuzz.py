29594aa6808f83238f6c1eac9c97637c33492168
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-10-18 14:02:26 +0200

    first 6 steps with failing test

--- kata/fizzbuzz.py
+++ kata/fizzbuzz.py
@@ -1,2 +1,6 @@
 def fizzbuzz(n):
+    if n == 3:
+        return 'Fizz'
+    if n == 5:
+        return 'Buzz'
     return n
--- kata/fizzbuzz_test.py
+++ kata/fizzbuzz_test.py
@@ -2,4 +2,17 @@ import pytest
 from kata.fizzbuzz import fizzbuzz
 
 def test_fizzbuzz():
-    assert fizzbuzz(0) == 0
+    assert fizzbuzz(1) == 1
+
+def test_fizzbuzz2():
+    assert fizzbuzz(2) == 2
+
+
+def test_fizzbuzz3():
+    assert fizzbuzz(3) == 'Fizz'
+
+def test_fizzbuzz5():
+    assert fizzbuzz(5) == 'Buzz'
+
+def test_fizzbuzz6():
+    assert fizzbuzz(6) == 'Fizz'
